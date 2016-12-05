7e42b87b3f7a4f853ebd4271d057fbb8b2ad85c0
lostezra <lostezra@gmail.com>
2016-12-05 16:02:27 +0800

    伪造了一些点数据

--- mockJson.js
+++ mockJson.js
@@ -48,6 +48,7 @@ var mockJson = {
         }
       ]
     },
+    
     {
       "pipeInfo":{
         "ID": 1114,
@@ -71,5 +72,166 @@ var mockJson = {
         }
       ]
     }
-  ]
+    
+    {
+      "pipeInfo":{
+        "ID": 111,
+        "name": "同济软院地下水管",
+        "type": "排水管",
+        "companyName": "水利局",
+        "status":1
+      },
+      "points":[
+        { x: 121.215758,
+          y: 31.2917621,
+          z: 50,
+          radius: 5,
+          location: "start"
+        }, //维新馆
+        { x: 121.218408,
+          y: 31.292888,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //图书馆
+        { x: 121.218862,
+          y: 31.294032,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //电信
+        { x: 121.217676,
+          y: 31.294259,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //材料
+        { x: 121.218421,
+          y: 31.295092,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //机械
+        { x: 121.217186,
+          y: 31.295089,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //汽车
+        { x: 121.216014,
+          y: 31.296359,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //土木实验室
+        { x: 121.217626,
+          y: 31.296294,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //风洞中心
+        { x: 
+          y: ,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //风洞中心
+        { x: 121.219872,
+          y: 31.296198,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //汽车能源中心
+        { x: 121.222931,
+          y: 31.296036,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, //体育馆
+        { x: 121.224777,
+          y: 31.296703,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #18
+        { x: 121.225999,
+          y: 31.296363,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #20
+        { x: 121.224373,
+          y: 31.295611,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #14#15
+        { x: 121.223668,
+          y: 31.294713,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #12
+        { x: 121.223488,
+          y: 31.293771,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // 食堂
+        { x: 121.223591,
+          y: 31.293066,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // 新天地教超
+        { x: 121.225078,
+          y: 31.292765,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #9#10
+        { x: 121.224633,
+          y: 31.292012,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #7#8
+        { x: 121.222823,
+          y: 31.291791,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // #H
+        { x: 121.220353,
+          y: 31.288527,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // 软件学院
+        { x: 121.220227,
+          y: 31.289361,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // 济人楼
+        { x: 121.218978,
+          y: 31.290194,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // 同德楼
+        { x: 121.218206,
+          y: 31.290171,
+          z: 50,
+          radius: 5,
+          location: "mid"
+        }, // 同心楼
+        { x: 121.218457,
+          y: 31.288389,
+          z: 50,
+          radius: 5,
+          location: "end"
+        } // 正门  
+      ]
+  	}
 }
